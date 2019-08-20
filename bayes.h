e4ed82a1b82ab6af7b90e170a99db3e742726929
root <root@localhost>
2019-08-20 18:40:50 +0800

    add sa2A

--- bayes.h
+++ bayes.h
@@ -4,11 +4,16 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-typedef struct  array{
-    int length;
-    int  *array;
+typedef struct array{
+    int len;
+    int *array;
 } Array;
 
-
+Array sa2A(int in[]){
+	Array t;
+	t.len=sizeof(in)/sizeof(int);
+	for(int i=0;i<t.len;i++)t.array[i]=in[i];
+	return t;
+}
 
 #endif
